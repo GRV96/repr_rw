d794c993c4bd3413d2102b87d04973ea85091c51
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-09-17 18:19:53 -0400

    Instructions in demo_read updated

--- demo_read.py
+++ demo_read.py
@@ -11,11 +11,11 @@ if not obj_path.exists():
 	print("Run demo_write.py to generate the file that this script needs.\n")
 	exit(1)
 
-# The first importation works with None because the
-# first entry in sys.path is the current directory.
+# The first importation works with None because the first
+# entry in sys.path is the current working directory.
 # To ensure that the function works with any
-# package, copy demo_package in another directory
-# and replace None with the new path.
+# package, move demo_package to another directory
+# and replace None with the path to that directory.
 importations = {
 	"from demo_package import Ajxo, Point": None,
 	"from pathlib import PosixPath, WindowsPath": None}
