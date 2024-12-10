a1120ddfa963c596e5b8fceae6183faf298f090c
Guyllaume Rousseau <32419088+GRV96@users.noreply.github.com>
2024-12-09 21:54:22 -0500

    Demos tested in the CI workflow (#12)
    The test ensures that no error occurs while the demos run.

---
+++ .github/workflows/test_demos_no_error.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+from os import system
+from pathlib import Path
+
+
+# This script is meant to run on GitHub's virtual machine ubuntu-latest in a
+# CI workflow. It runs the repository's demos to ensure that no error occurs.
+
+repo_root = Path(__file__).resolve().parents[2]
+demo_dir = repo_root/"demos"
+
+# demo_write.py must be executed first because
+# it produces a file that demo_read.py needs.
+system(f"python3 {demo_dir}/demo_write.py")
+system(f"python3 {demo_dir}/demo_read.py")
--- .github/workflows/test_pypi_package_build.py
+++ .github/workflows/test_pypi_package_build.py
@@ -4,15 +4,14 @@ from os import system
 from pathlib import Path
 
 
-_REPO_ROOT = Path(__file__).resolve().parents[2]
-
-
 # This script is meant to run on GitHub's virtual machine ubuntu-latest in a CI
 # workflow. It builds the PyPI package and installs it to ensure the
 # installation works. However, the script does not upload the package to PyPI.
 
-system(f"python3 {_REPO_ROOT}/setup.py sdist")
+repo_root = Path(__file__).resolve().parents[2]
 
-latest_dist = list((_REPO_ROOT/"dist").glob("repr_rw-*.tar.gz"))[-1]
+system(f"python3 {repo_root}/setup.py sdist")
+
+latest_dist = list((repo_root/"dist").glob("repr_rw-*.tar.gz"))[-1]
 
 system(f"pip3 install --no-cache-dir {latest_dist}")
