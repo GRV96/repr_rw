764442d4150ebef0c5fc06752a0f01356167dfbf
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-12-09 21:48:23 -0500

    A comment explains why `demo_write.py` must be executed first.

--- .github/workflows/test_demos_no_error.py
+++ .github/workflows/test_demos_no_error.py
@@ -10,5 +10,7 @@ _DEMO_DIR = _REPO_ROOT/"demos"
 # This script is meant to run on GitHub's virtual machine ubuntu-latest in a
 # CI workflow. It runs the repository's demos to ensure that no error occurs.
 
+# demo_write.py must be executed first because
+# it produces a file that demo_read.py needs.
 system(f"python3 {_DEMO_DIR}/demo_write.py")
 system(f"python3 {_DEMO_DIR}/demo_read.py")
