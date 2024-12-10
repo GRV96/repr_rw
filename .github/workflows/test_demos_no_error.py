fb45b0fc3255c7d90f15fbdbac925aaaae1c789d
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-12-09 21:52:47 -0500

    Constants replaced with variables

--- .github/workflows/test_demos_no_error.py
+++ .github/workflows/test_demos_no_error.py
@@ -4,13 +4,13 @@ from os import system
 from pathlib import Path
 
 
-_REPO_ROOT = Path(__file__).resolve().parents[2]
-_DEMO_DIR = _REPO_ROOT/"demos"
-
 # This script is meant to run on GitHub's virtual machine ubuntu-latest in a
 # CI workflow. It runs the repository's demos to ensure that no error occurs.
 
+repo_root = Path(__file__).resolve().parents[2]
+demo_dir = repo_root/"demos"
+
 # demo_write.py must be executed first because
 # it produces a file that demo_read.py needs.
-system(f"python3 {_DEMO_DIR}/demo_write.py")
-system(f"python3 {_DEMO_DIR}/demo_read.py")
+system(f"python3 {demo_dir}/demo_write.py")
+system(f"python3 {demo_dir}/demo_read.py")
