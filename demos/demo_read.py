26e20f36646d1647ec7b053da262925700e74996
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-12-29 14:29:11 -0500

    `SysPathBundle` can handle the paths out of `read_reprs`

--- demos/demo_read.py
+++ demos/demo_read.py
@@ -2,7 +2,8 @@ from pathlib import Path
 import sys
 from syspathmodif import\
 	sp_append,\
-	sp_remove
+	sp_remove,\
+	SysPathBundle
 
 _LOCAL_DIR = Path(__file__).parent.resolve()
 _REPO_ROOT = _LOCAL_DIR.parent
@@ -26,9 +27,18 @@ importations = [
 	"from pathlib import PosixPath, WindowsPath"
 	# Built-in types tuple and list do not require a path.
 ]
-paths = [_REPO_ROOT, _REPO_ROOT/"demo_package"]
+paths = (_REPO_ROOT, _REPO_ROOT/"demo_package")
 obj_generator = read_reprs(obj_path, importations, paths)
 
 print("Objects read:")
 for obj in obj_generator:
 	print(f"{obj} {type(obj)}")
+
+obj_generator = None
+with SysPathBundle(paths):
+	obj_generator = read_reprs(obj_path, importations)
+
+if obj_generator is not None:
+	print("\nObjects read:")
+	for obj in obj_generator:
+		print(f"{obj} {type(obj)}")
