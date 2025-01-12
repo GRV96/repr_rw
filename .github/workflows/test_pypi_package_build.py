927bd871f970ade568520654d33dc4bb3be52121
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-01-12 00:38:49 -0500

    Script `test_pypi_package_build.py` has a docstring.

--- .github/workflows/test_pypi_package_build.py
+++ .github/workflows/test_pypi_package_build.py
@@ -1,10 +1,13 @@
+"""
+This script is meant to run on GitHub's virtual machine ubuntu-latest in a CI
+workflow. It builds the PyPI package and installs it to ensure the
+installation works. However, the script does not upload the package to PyPI.
+"""
+
+
 from os import system
 from pathlib import Path
 
-
-# This script is meant to run on GitHub's virtual machine ubuntu-latest in a CI
-# workflow. It builds the PyPI package and installs it to ensure the
-# installation works. However, the script does not upload the package to PyPI.
 
 repo_root = Path(__file__).resolve().parents[2]
 
